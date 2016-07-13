8f1f5342f6a1f7a97a29ab5e0f1e730874716eb0
Rudolf Olah <omouse@gmail.com>
2016-07-13 12:43:46 -0400

    moved mosaico-example20
    into sub-directory for docker-ized testing

--- docker-test-examples.sh
+++ docker-test-examples.sh
@@ -1,4 +1,4 @@
-for example in simple complex yeoman-generated-issue2
+for example in simple complex yeoman-generated-issue2 mosaico-issue20
 do
     rm -f "./examples/$example/gulpfile.js"
     docker run --rm -v $(realpath "./examples/$example/"):/data/package omouse/grunt2gulp.js
